0e7c63b1e4ca36da81e95b8c9ca964da5f1d1c6f
J.Ordan <giordano@scalzo.biz>
2009-10-12 17:47:53 +0200

    final version refactored

--- lib/average_time_of_day.rb
+++ lib/average_time_of_day.rb
@@ -1,38 +1,50 @@
 require 'time'
 
-def average_time_of_day(times)
-
-	times_in_time = times.map { |t| 
-		Time.parse(t)
-	}
-	times_am = times_in_time.select { |t|
-		t.hour < 12
-	}
-
-	avg_am = times_am.map { |t|
-		t.to_f
-	}.inject(0.0){|sum,el| sum + el}/times_am.size
-
-	times_pm = times_in_time.select { |t|
-		t.hour >= 12
-	}
-	avg_pm = times_pm.map { |t|
-		t.to_f
-	}.inject(0.0){|sum,el| sum + el}/times_pm.size
-
-	if(avg_pm - avg_am  >= 12*3600) then
-		times_in_time = times_in_time.map { |t|
-			(t.hour < 12 ? t+60*60*24 : t)
-		}
+private
+class Array
+	def to_sec
+		map {|t|t.to_f}
 	end
 
-	times_in_sec = times_in_time.map { |t|
-		t.to_f
-	}
-	min = times_in_sec.min
+	def avg
+		to_sec.inject(0.0){|sum,el| sum + el}/size
+	end
+end
 
-	ranges = times_in_sec.map { |r| r-min }
+class Time
+	def am?
+		hour < 12	
+	end
 
-	avg = ranges.inject(0.0){|sum,el| sum + el}/ranges.size
-	Time.at(min+avg).strftime("%I:%M%p").downcase
+	def pm?
+		not am?
+	end
+
+	def to_s
+		strftime("%I:%M%p").downcase
+	end
+	
+end
+
+DAY_IN_SEC = 24*60*60
+
+def adjust_order_for(times)
+	to_adjust?(times) ? adjusted(times) : times
+end
+
+def to_adjust?(times)
+	avg_am = times.select { |t| t.am? }.avg
+	avg_pm = times.select { |t| t.pm? }.avg
+	avg_pm - avg_am  >= DAY_IN_SEC/2
+end
+
+def adjusted(times)
+	times.map { |t|(t.am? ? t+DAY_IN_SEC : t) }
+end
+
+public
+def average_time_of_day(times)
+	times = adjust_order_for(times.map { |t| Time.parse(t) })
+
+	Time.at(times.avg).to_s
 end
